783eebf8e9ac454c302ef9b7768e09b6a528fa28
chetjones003 <chetjones003@gmail.com>
2022-09-05 12:25:46 -0500

    Draw a red box using pygame.surface

--- IntroToPygame/main.py
+++ IntroToPygame/main.py
@@ -10,11 +10,17 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
+# first surface
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    screen.blit(test_surface, (200,100))
 
     # draw all elements and update everything
     pygame.display.update()
