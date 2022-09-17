ea0e277ebbd41f89d0c025d8b8f901a104f0bfd2
chetjones003 <chetjones003@gmail.com>
2022-09-16 21:41:37 -0500

    Use rectangles to get positional information

--- IntroToPygame/main.py
+++ IntroToPygame/main.py
@@ -6,20 +6,17 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(os.path.join('font', 'Pixeltype.ttf'), 50)
+test_font = pygame.font.Font(os.path.join('IntroToPygame', 'font', 'Pixeltype.ttf'), 50)
 
-sky_surface = pygame.image.load(os.path.join(
-    'graphics', 'Sky.png'
-    )).convert()
-ground_surface = pygame.image.load(os.path.join(
-    'graphics', 'ground.png'
-    )).convert()
+sky_surface = pygame.image.load(os.path.join('IntroToPygame', 'graphics', 'Sky.png')).convert()
+ground_surface = pygame.image.load(os.path.join('IntroToPygame', 'graphics', 'ground.png')).convert()
 text_surface = test_font.render('My Game', False, 'Black')
 
-snail_surface = pygame.image.load(os.path.join(
-    'graphics', 'snail1.png'
-    )).convert_alpha()
-snail_x_pos = 600
+snail_surface = pygame.image.load(os.path.join('IntroToPygame', 'graphics', 'snail1.png')).convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom=(600, 300))
+
+player_surface = pygame.image.load(os.path.join('IntroToPygame', 'graphics', 'player_stand.png')).convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -31,11 +28,12 @@ while True:
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
 
-    snail_x_pos -= 4
-    if snail_x_pos <= -100:
-        snail_x_pos = 800
+    snail_rect.x -= 4
+    if snail_rect.x <= -100:
+        snail_rect.x = 800
 
-    screen.blit(snail_surface, (snail_x_pos, 250))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
 
     # draw all elements and update everything
     pygame.display.update()
